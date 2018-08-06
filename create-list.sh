8529fbd0880e30fd7882d88c422edbb77eced87f
tsemach <tsemach.mizrachi>
2018-08-06 23:59:49 +0300

    add project.list

---
+++ create-list.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+project='angular-skeletoetonn'
+find . | sed -e 's/^\.\///g' | grep -v node_modules > project.list.all
+rm -f project.list
+
+for f in `cat project.list.all`
+do
+	git ls-files --error-unmatch $f > /dev/null 2>&1
+	if [ $? -eq 0 ] 
+	then
+		if [ -f $f ]; then
+			echo $f >> project.list
+		fi
+	fi 
+done
+
+rm -f project.list.all
